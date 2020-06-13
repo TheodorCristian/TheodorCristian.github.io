bf51509e4c08cca6167ecb0289260be56fe8a25e
TheodorCristian <theodorc.petre@gmail.com>
2020-06-13 23:26:16 +0300

    updates

--- Js/presentationGallery.js
+++ Js/presentationGallery.js
@@ -1,31 +1,31 @@
 $(document).ready(function(){
 
-    //Setting variables
+    // //Setting variables
 
-    let slider = document.querySelector('.presentation-slider');
-    let image = $('.presentation-image');
-    let width = image.innerWidth();
-    let counter = 1;
+    // let slider = document.querySelector('.presentation-slider');
+    // let image = $('.presentation-image');
+    // let width = image.innerWidth();
+    // let counter = 1;
 
-    //Setting image width transition
-    slider.style.transform = 'translateX(' + (-width * counter) + 'px)';
+    // //Setting image width transition
+    // slider.style.transform = 'translateX(' + (-width * counter) + 'px)';
 
-    setInterval(function(){
-        if (counter >= slider.length){
-            return;
-        } else {
-            slider.style.transition = 'transform .5s ease-in-out';
-            counter++;
-            slider.style.transform = 'translateX(' + (-width * counter) + 'px)';
-        };
-        slider.addEventListener('transitionend', function () {
-            if (image[counter].id === 'firstClone') {
-              slider.style.transition = 'none';
-              counter = image.length - counter;
-              slider.style.transform = 'translateX(' + (-width * counter) + 'px)';
-            }
-        });
-    }, 3000)
+    // setInterval(function(){
+    //     if (counter >= slider.length){
+    //         return;
+    //     } else {
+    //         slider.style.transition = 'transform .5s ease-in-out';
+    //         counter++;
+    //         slider.style.transform = 'translateX(' + (-width * counter) + 'px)';
+    //     };
+    //     slider.addEventListener('transitionend', function () {
+    //         if (image[counter].id === 'firstClone') {
+    //           slider.style.transition = 'none';
+    //           counter = image.length - counter;
+    //           slider.style.transform = 'translateX(' + (-width * counter) + 'px)';
+    //         }
+    //     });
+    // }, 3000)
 
     let mySwiperAnimation = new Swiper('.swiper-container', {
         effect: 'coverflow',
